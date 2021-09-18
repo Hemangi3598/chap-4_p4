025cdc2af98b60e2c0d4ba6b15edfac70665844d
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 15:59:14 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,18 @@
+  # wapp to declare and print strings 
+# single line string
+s1 = " kaml classes thane"
+print(s1)
+
+s2 = ' kamal classes borivali '
+print(s2)
+
+# multiline strings 
+s3 = """ 404, ishan arcade - 2
+above vidyalankar classes beside 
+thane west """
+print(s3)
+
+s4 = ''' 301, pbc,
+karter roadno. 1 
+borivali east '''
+print(s4)
